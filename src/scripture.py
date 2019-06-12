c20bdcddde5717a953ec85864c0ce2321a5181f6
Diego Lin <diegol@synnex.com>
2019-06-12 15:47:28 +0800

    Use the txt file under the same DIR with py file

--- src/scripture.py
+++ src/scripture.py
@@ -1,24 +1,26 @@
 #!/bin/env python
 
 from datetime import datetime
+import os
 import re
 
-
-#filename = "scripture.txt"
-filename = "/storage/emulated/0/documents/scripture.txt"
+pyfile = os.path.abspath(__file__)
+print(pyfile)
+pydir = os.path.dirname(pyfile)
+filename = pydir + "/scripture.txt"
+print(filename)
 with open(filename,encoding='UTF-8') as f:
     scripture = f.read()
 
-print(scripture)
+##print(scripture)
 
 now = datetime.now()
 mm = now.strftime('%m')
 dd = now.strftime('%d')
-all = f'{mm}月{dd}号感动经文:'
+all = f'{mm}月{dd}号感动经文:\n\n'
 line = re.sub(r'[\n\s]+|（.+）','',scripture)
 cleanStr = re.sub(r'和合本\)',r")\n\n",line)
-#all += '\n\n'
 all += cleanStr
-    
+
 print(all)
-print(111)
+print("777LJC777")
