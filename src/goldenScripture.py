d6ef7708419feff55ed26bac64da158689da4b28
Diego Lin <diegol@synnex.com>
2019-08-28 14:20:13 +0800

    All feature with Chs and Eng generated.

--- src/goldenScripture.py
+++ src/goldenScripture.py
@@ -5,20 +5,19 @@
 from datetime import datetime
 import os,re
 
+scripture = '''将列国的神像都扔在火里；因为它本不是神，乃是人手所造的，是木头石头的，所以灭绝它。 (列王纪下 19:18 和合本)
+They have thrown their gods into the fire and destroyed them, for they were not gods but only wood and stone, fashioned by human hands. (2 Kings 19:18 NIV)'''
+
 linesep = os.linesep
-scripture = '''耶和华－你　神原是有怜悯的　神；他总不撇下你，不灭（不撇下你，不）绝你，也不忘记他起誓   与你列祖所立的约。  (申命记 4:31 和合本)
-For the Lord your God is a merciful God; he will not abandon or destroy you or forget the covenant with your ancestors, which he confirmed to them by oath.  (Deuteronomy 4:31 NIV)'''
-
-
 now = datetime.now()
 mm = now.strftime('%m')
 dd = now.strftime('%d')
-allSc = f'{mm}月{dd}号背诵金句: '
-#08月27号背诵金句: 126415  9147
+allSc = f'{mm}月{dd}号背诵金句: '  ## 08月27号背诵金句: 126415  9147
 
-chEng = scripture.split('和合本)')
-chs = re.sub(r'[\n\s]+|（.+?）','',chEng[0]) + ')'
+chEng = scripture.split('和合本)')  ## Seperate Chinese from English
+chs = re.sub(r'[\n\s]+|（.+?）','',chEng[0]) + ')'  ## Clean Chinese part
 
+## Count Chinese characters
 chsParts = re.split(r'[，。；－]',chs)
 count = ""
 for part in chsParts:
@@ -27,58 +26,20 @@ for part in chsParts:
     count += str(len(part))
     
 #print(count)
-allSc += count + linesep + linesep + chs + linesep
-#print(allSc)
-eng = re.sub(r'\s*\((\w+) ([\d:]+) NIV\)',r'(\1\2)',chEng[1])
-eng = re.sub(linesep+'*','',eng)
-allSc += eng
-#print(eng)
-#print(1111111111)
+
+## Handle Eng
+eng = re.sub(r'\s*\(((?:[12] )?\w+) ([\d:]+) NIV\)',r'(\1\2)',chEng[1])  ## Handle the part '. (2 Kings 19:18 NIV)'
+eng = re.sub(linesep+'*','',eng)  ## Clean Eng part
+
+## Count Eng words
+engParts = re.split(r'[,.;]',eng)
+count += "  "
+for part in engParts:
+    if part.startswith('('):
+        continue
+    #print(part.strip().split(' '))
+    count += str(len(part.strip().split(' ')))
+allSc += count + linesep + linesep + chs + linesep + eng
 print(allSc)
-exit()
-result = re.match(r'(.+)\(.+和合本\)(.+)',scripture)
-print(result)
-if True:
-    quit()
-now = datetime.now()
-mm = now.strftime('%m')
-dd = now.strftime('%d')
-all = f'{mm}月{dd}号背诵金句: '
-
-line = re.sub(r'[\n\s]+|（.+）|和合本|NIV','',scripture)
-parts = re.split('[；，。]',line)
-for part in parts:
-    cleanStr = re.sub(r'[：“、…]','',part)
-    account = str(len(cleanStr))
-    all += account
-    print(account,part)
-print(all)
-
-
-
-###
-# re.subn(r'\(.+?\)\s*','',sc)
-##('Let us not become weary in doing good, for at the proper time we, will reap a harvest if we do not give up. ', 2)
-'''
-ss='Let us not become weary in doing good, for at the proper time (right time) we(good), will reap a harvest if we do not give up. (Galatians 6:9 NIV)'
-
-
-chapNoPtn = re.compile(r'\(\w+\s*\d+:\d+(?:-\d+)?(?:\s+NIV)?\)')
-chapNoPtn.findall(ss)
-['(Galatians 6:9 NIV)']
->>> chapNoPtn.sub('',ss)
-'Let us not become weary in doing good, for at the proper time (right time) we(good), will reap a harvest if we do not give up. '
->>> sc = chapNoPtn.sub('',ss)
->>> sc
-'Let us not become weary in doing good, for at the proper time (right time) we(good), will reap a harvest if we do not give up. '
-
->>> re.sub(r'\(.+?|)\s','',sc)
->>> re.sub(r'\(.+?\)\s','',sc)
-'Let us not become weary in doing good, for at the proper time we(good), will reap a harvest if we do not give up. '
->>> re.subn(r'\(.+?\)\s*','',sc)
-('Let us not become weary in doing good, for at the proper time we, will reap a harvest if we do not give up. ', 2)
-
-
-'''
 ##==== Glory to GOD ====
 
