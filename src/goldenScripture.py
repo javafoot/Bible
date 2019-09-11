fdd722732bc89cc562d48a65571e12ad4c10fe18
Diego Lin <diegol@synnex.com>
2019-09-11 13:43:44 +0800

    Handle the quote mark in English scripture.

--- src/goldenScripture.py
+++ src/goldenScripture.py
@@ -5,7 +5,7 @@
 from datetime import datetime
 import os,re
 
-scripture = '''所以你当归向你的　神，谨守仁爱、公平，常常等候你的　神。(何西阿书 12:6 和合本)But you must return to your God; maintain love and justice, and wait for your God always. (Hosea 12:6 NIV)'''
+scripture = '''如经上所记：　神为爱他的人所预备的是眼睛未曾看见，耳朵未曾听见，人心也未曾想到的。(哥林多前书 2:9 和合本)However, as it is written: “What no eye has seen, what no ear has heard, and what no human mind has conceived”— the things God has prepared for those who love him. (1 Corinthians 2:9 NIV)'''
 
 linesep = os.linesep
 now = datetime.now()
@@ -14,10 +14,11 @@ dd = now.strftime('%d')
 allSc = f'{mm}月{dd}号背诵金句: '  ## 08月27号背诵金句: 126415  9147
 
 chEng = scripture.split('和合本)')  ## Seperate Chinese from English
-chs = re.sub(r'[\n\s]+|（.+?）','',chEng[0]) + ')'  ## Clean Chinese part
 
+## Handle Chinese
+chs = re.sub(r'[\n\s]+|（.+?）','',chEng[0]) + ')'  ## Clean Chinese part
 ## Count Chinese characters
-chsParts = re.split(r'[，。；－”]+',chs)
+chsParts = re.split(r'[，。；－：”]+',chs)
 print(chsParts)
 count = ""
 for part in chsParts:
@@ -25,15 +26,14 @@ for part in chsParts:
         continue
     partWithoutPuncMark = re.sub(r'、','',part)  ## Remove the punctuation marks like '、' before counting.
     count += str(len(partWithoutPuncMark))
-    
 print(count)
 
 ## Handle Eng
 eng = re.sub(r'\s*\(((?:[12] )?\w+) ([\d:]+) NIV\)',r'(\1\2)',chEng[1])  ## Handle the part '. (2 Kings 19:18 NIV)'
 eng = re.sub(linesep+'*','',eng)  ## Clean Eng part
-
 ## Count Eng words
-engParts = re.split(r'[,.;]',eng)
+engParts = re.split(r'[,.;]|: “|”—',eng)
+print(engParts)
 count += "  "
 for part in engParts:
     if part.startswith('('):
